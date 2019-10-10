3e5cc1d86ca3c71367291275f9ec2cf444c4d416
Ryan Propst <ryanpropst@gmail.com>
2019-10-10 08:52:17 -0500

    corrected title

---
+++ experiment.js
@@ -0,0 +1,44 @@
+// const avaiable = ""
+
+// class Product {
+//   get isInStock() {
+//     return this.inventoryAvaiable > 0;
+//   }
+
+//   get stockLevel() {
+//     return this.inventoryAvaiable || 0;
+//   }
+
+//   constructor(name, id, stock_level, price, taxed) {
+//     this.name = name;
+//     this.id = id;
+//     this.inventoryAvaiable = stock_level;
+//     this.price = price;
+//     this.taxed = [true, false];
+//     this.brand;
+//     this.model;
+//     this.vendor;
+//     this.sku;
+//     this.avaiable = true;
+//   }
+
+//   addToCart() {
+//     alert(`{this.name} has been added to your cart`);
+//   }
+
+//   checkStockLevel() {}
+
+//   handlePrice() {}
+
+//   markAsUnavaiablle() {
+
+//   }
+// }
+
+// class Shoe extends Product {
+//   constructor(price) {
+//     super(price);
+//   }
+// }
+
+// console.log(new Shoe(1.99));
--- shopping.js
+++ shopping.js
@@ -1,1 +1,69 @@
-class ()
+
+class ShoppingCart {
+  constructor(cart) {
+    // this.products = []; // could make it an array but an object is better
+    // this.products = {
+    //   name:null,
+    //   prod_id:null,
+    //   price:null
+
+    // };
+    this.cart = {}};
+  }
+
+  addProduct(producs) {
+    // if (!this.price )
+    // console.log('inside addProducts');
+    // console.log(products);
+    if(!this.cart[products.id]){
+      this.cart[products.id] = {
+      quantity: 1,
+      product: products,
+      };
+    } else {
+      this.cart[products.id].quantity++;
+    }
+
+updateQanity(id, quantity ){
+  this.cart[id].quantity = quantity;
+}
+
+  removeProduct(id, all=true , quantity) {}
+
+  // addProducts(products) {        // one option
+  //   products.forEach((product) => {
+  //     this.addProduct(product);
+  //   });
+  // }
+
+  caclTotalPrice() {}
+
+  checkDiscountCode() {}
+  }
+
+class Watch {
+  constructor(opts) {
+    this.price = opts.price;
+    this.brand=  opts.brand;
+    this.sku = opts.sku;
+    this.description = opts.description;
+    this.inventoryCount = opts.inventoryCount;
+    this.id = opts.id;
+  }
+}
+
+// all cost are in pennies
+const watch = new Watch({
+  price: 100000,
+  brand: 'Weiss Watch Company',
+  sku: '1984DA89AEQR20',
+  description: 'This is a cool watch',
+  inventoryCount: 50,
+  id: '090914097'
+
+});
+
+const cart = new ShoppingCart();
+
+cart.addProduct(watch);
+cart.removeProduct(watch);
